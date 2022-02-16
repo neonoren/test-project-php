4707b41e5c5043657a300b79481c34951fc1b8a6
neonoren <neonoren@gmail.com>
2022-02-16 16:53:55 +0500

    second commit

--- insert.php
+++ insert.php
@@ -3,4 +3,14 @@
 $firstname = $_POST['firstname'];
 $lastname = $_POST['lastname'];
 
-$co
+$connect = mysqli_connect('localhost', 'root', '', 'phpfact');
+
+$query = "INSERT INTO users (firstname, lastname) VALUE ('$firstname', '$lastname')";
+
+$result = mysqli_query($connect,$query);
+
+mysqli_close($connect);
+
+?>
+
+<meta http-equiv="refresh" content="1; url=index.php">
